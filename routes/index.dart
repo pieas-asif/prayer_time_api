30001158ba93903541cbbaadf9523b13c6f9fed1
Shaman <pieas.asif4@gmail.com>
2023-03-24 15:30:02 +0600

    version update

--- routes/index.dart
+++ routes/index.dart
@@ -2,6 +2,6 @@ import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
   return Response.json(
-    body: {'service': 'Prayer Time API', 'version': '0.9.0'},
+    body: {'service': 'Prayer Time API', 'version': '0.9.5'},
   );
 }
